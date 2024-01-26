93e6558a1b742998d77de23b667d6e9d5e9c271d
GiulioSurya <153427674+GiulioSurya@users.noreply.github.com>
2024-01-26 12:07:24 +0100

    Update scrapingloop.r
    Added tryCatch to handle the 403 error. There's still an issue with tracks_links[6]: the output is NA because there are no pages at that URL

--- scrapingloop.r
+++ scrapingloop.r
@@ -1,65 +1,62 @@
 library(tidyverse)
 library(rvest)
+library(googlesheets4)
 
-#setwd for useragents, i will upload on web when i will finish
-setwd("C:/Users/c_ans/Desktop/bologna lezioni/comunication of statistics")
-ualist<-read.table("useragents.txt", sep = "\n")
-#create a list of links with all the possible songs
-page <- read_html("https://www.whosampled.com/Daft-Punk/sampled/?role=1", user_agent=sample(ualist))
-tracks_links <- page %>% html_nodes(".trackCover") %>%
-    html_attr("href") %>% paste0("https://www.whosampled.com", .,"sampled/")
+#user agent list
+ualist<-read_sheet("https://docs.google.com/spreadsheets/d/1FduIFZUJcPFzKWm2qxdQw4qZo1nJwnLf9n5ddtR1w5A/edit#gid=0")
+#first url (i need it to create the list of all URls)
+page <- read_html("https://www.whosampled.com/Daft-Punk/sampled/?role=1", user_agent = sample(ualist))
 
+#create the list of urls
+tracks_links <- page %>% 
+  html_nodes(".trackCover") %>%
+  html_attr("href") %>%
+  paste0("https://www.whosampled.com", ., "sampled/") 
 
-
-#first method
-#loop for each link
-for (i in length(tracks_links)){
-    #read the link and find the number of pages
-    page <- read_html(tracks_links[i], user_agent=sample(ualist))
-    pages <- page %>% 
-  html_elements(".page a") %>% 
-  html_text2() %>% 
-  last()
-  #extract the data from each page
-    paste0(tracks_links[i], "?cp=", 1:pages) %>%
-        map(~read_html(.,user_agent)) %>% 
-        map_dfr(~ html_elements(.x, ".table.tdata tbody tr") %>% 
-                    map_dfr(~ tibble(
-                        title = html_element(.x, ".trackName.playIcon") %>% 
-                            html_text2(),
-                        artist = html_element(.x, ".tdata__td3") %>% 
-                            html_text2(),
-                        year = html_element(.x, ".tdata__td3:nth-child(4)") %>% 
-                            html_text2(),
-                        genre = html_element(.x, ".tdata__badge") %>% 
-                            html_text2()
-                    ))) -> tracks
+#loop for each url
+for (i in 1:length(tracks_links)) {
+  link <- tryCatch({                                              #tryCatch is used to handle error 403
+    read_html(tracks_links[6], user_agent = sample(ualist))
+  }, error = function(e) {
+    cat("Error occurred, retrying...\n")
+    Sys.sleep(5)
+    NULL
+  })
+  
+  if (!is.null(link)) {
+    links <- link %>% 
+      html_elements(".page a") %>% 
+      html_text2() %>% 
+      last() 
+    
+    result <- paste0(tracks_links[i], "?cp=", 1:links) %>%  
+      map(function(url) {
+        tryCatch({
+          read_html(url, user_agent = sample(ualist))
+        }, error = function(e) {
+          cat("Error occurred in nested read_html, retrying...\n")
+          Sys.sleep(5)
+          NULL
+        })
+      }) %>%
+      map_dfr(~ {
+        if (!is.null(.x)) {
+          html_elements(.x, ".table.tdata tbody tr") %>% 
+            map_dfr(~ tibble(
+              title = html_element(.x, ".trackName.playIcon") %>% 
+                html_text2(),
+              artist = html_element(.x, ".tdata__td3") %>% 
+                html_text2(),
+              year = html_element(.x, ".tdata__td3:nth-child(4)") %>% 
+                html_text2(),
+              genre = html_element(.x, ".tdata__badge") %>% 
+                html_text2() 
+            ))
+        } else {
+          tibble()  # Restituisce un tibble vuoto se .x è NULL
+        }
+      })
+  }
 }
-
-
-#second method
-pages_list <- list()
-#create a list with all the links and pages
-for (i in seq_along(tracks_links)) {
-  page<- read_html(tracks_links[1], user_agent = sample(ualist))
-  pages <- page %>% 
-    html_elements(".page a") %>% 
-    html_text2() %>% 
-    last()
-  pages_list[[i]] <- paste0(tracks_links[i], "?cp=", 1:as.numeric(pages))
-}
-#extract the data from each page
-tracks <- map_df(pages_list, ~read_html(.x) %>%
-                    html_elements(".table.tdata tbody tr") %>% 
-                    map_dfr(~ tibble(
-                      title = html_element(.x, ".trackName.playIcon") %>% 
-                        html_text2(),
-                      artist = html_element(.x, ".tdata__td3") %>% 
-                        html_text2(),
-                      year = html_element(.x, ".tdata__td3:nth-child(4)") %>% 
-                        html_text2(),
-                      genre = html_element(.x, ".tdata__badge") %>% 
-                        html_text2()
-                    )))
 
 
