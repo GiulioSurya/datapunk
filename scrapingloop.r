d59ce99c9592e7189e4261aa82a5582eb377fb72
GiulioSurya <153427674+GiulioSurya@users.noreply.github.com>
2024-01-26 15:28:26 +0100

    Update scrapingloop.r

--- scrapingloop.r
+++ scrapingloop.r
@@ -6,15 +6,16 @@ library(googlesheets4)
 ualist<-read_sheet("https://docs.google.com/spreadsheets/d/1kS6VkrUu7txADr-4xs1M2SREv1YD9HoWEbUOr_8RdG0/edit#gid=0")
 
 #create a list of links with all the possible daft punk songs
-tracks_links<-list()
+tracks_links<-c()
 for (i in 1:8) {
-   page<- read_html(paste0("https://www.whosampled.com/Daft-Punk/sampled/?role=",i ),user_agent = sample(ualist))
+   page<- read_html(paste0("https://www.whosampled.com/Daft-Punk/sampled/?role=1&sp=",i),user_agent = sample(ualist))
    pages<- page %>% 
      html_nodes(".trackCover") %>%
      html_attr("href") %>%
      paste0("https://www.whosampled.com", ., "sampled/") 
  tracks_links <- c(tracks_links, pages)
 }
+
 
 #create a tibble to store the data
 result <- tibble(
@@ -26,21 +27,20 @@ result <- tibble(
 
 #loop for each url tracks_links
 for (i in 1:length(tracks_links)) {
-  link <- tryCatch({                                        #try catch is used to avoid error 403
-    read_html(tracks_links[i], user_agent = sample(ualist))     
+  link <- tryCatch({
+    read_html(tracks_links[i], user_agent = sample(ualist))
   }, error = function(e) {
     cat("Error occurred, retrying...\n")
     Sys.sleep(5)
     NULL
   })
-  
   if (!is.null(link)) {
     links <- link %>% 
       html_elements(".page a") %>% 
       html_text2() %>% 
       last() 
-      if (is.null(links)) {                            #some urls in tracks_links don't have more than one page
-      links <- 1                                       #this assign 1 to links if it is null
+    if (is.null(links)) {
+      links <- 0
     }
     extracted<- paste0(tracks_links[i], "?cp=", 1:links) %>%  
       map(function(url) {
