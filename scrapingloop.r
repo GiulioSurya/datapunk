00de54c53ede24fe5cd861851d4dc9986eafa816
GiulioSurya <153427674+GiulioSurya@users.noreply.github.com>
2024-01-30 20:57:15 +0100

    Update scrapingloop.r

--- scrapingloop.r
+++ scrapingloop.r
@@ -1,23 +1,38 @@
 library(tidyverse)
 library(rvest)
 library(googlesheets4)
+library(httr)
 
-#create a user agent list to avoid error 403 when a request is made to the server
-ualist<-read_sheet("https://docs.google.com/spreadsheets/d/1kS6VkrUu7txADr-4xs1M2SREv1YD9HoWEbUOr_8RdG0/edit#gid=0")
+#create an user agent list
+ualist<-read_sheet("https://docs.google.com/spreadsheets/d/1FduIFZUJcPFzKWm2qxdQw4qZo1nJwnLf9n5ddtR1w5A/edit#gid=0")
 
-#create a list of links with all the possible daft punk songs
+#select an user agent that works
+ua_success <- character()
+for(i in 200:499) {
+  response <- httr::GET("https://www.whosampled.com/Daft-Punk/sampled/?role=1", 
+                        add_headers(`User-Agent` = as.character(ualist[i,])))
+  if(response$status_code == 200) {
+    ua_success <- c(ua_success, as.character(ualist[i,]))
+    break
+  }
+  Sys.sleep(10)
+}
+
+
+#extract the links of the pages
 tracks_links<-c()
 for (i in 1:8) {
-   page<- read_html(paste0("https://www.whosampled.com/Daft-Punk/sampled/?role=1&sp=",i),user_agent = sample(ualist))
-   pages<- page %>% 
+  pages<-GET(paste0("https://www.whosampled.com/Daft-Punk/sampled/?role=1&sp=",i), user_agent(ua_success)) %>%
+  content(type = "text/html") %>% 
      html_nodes(".trackCover") %>%
      html_attr("href") %>%
      paste0("https://www.whosampled.com", ., "sampled/") 
  tracks_links <- c(tracks_links, pages)
+ Sys.sleep(60)
 }
 
 
-#create a tibble to store the data
+#inizialized the result
 result <- tibble(
   title = character(),
   artist = character(),
@@ -25,36 +40,21 @@ result <- tibble(
   genre = character()
 )
 
-#loop for each url tracks_links
+#extract the data
 for (i in 1:length(tracks_links)) {
-  link <- tryCatch({
-    read_html(tracks_links[i], user_agent = sample(ualist))
-  }, error = function(e) {
-    cat("Error occurred, retrying...\n")
-    Sys.sleep(5)
-    NULL
-  })
-  if (!is.null(link)) {
-    links <- link %>% 
+Sys.sleep(10)
+links<-GET(tracks_links[i], user_agent(ua_success)) %>%
+  content(type = "text/html") %>% 
       html_elements(".page a") %>% 
       html_text2() %>% 
-      last() 
-    if (is.null(links)) {
-      links <- 0
-    }
-    extracted<- paste0(tracks_links[i], "?cp=", 1:links) %>%  
-      map(function(url) {
-        tryCatch({
-          read_html(url, user_agent = sample(ualist))
-        }, error = function(e) {
-          cat("Error occurred in nested read_html, retrying...\n")
-          Sys.sleep(5)
-          NULL
-        })
-      }) %>%
-      map_dfr(~ {
-        if (!is.null(.x)) {
-          html_elements(.x, ".table.tdata tbody tr") %>% 
+      last()
+  if (is.na(links)) {
+ extracted<-tracks_links[i]
+  }  else {
+    extracted<- paste0(tracks_links[i], "?cp=", 1:links)
+  }   
+    extracted_data<- map(extracted,~GET(.x,user_agent(ua_success))) %>% map(~content(.,type = "text/html")) %>% 
+    map_dfr(~ html_elements(.x, ".table.tdata tbody tr") %>% 
             map_dfr(~ tibble(
               title = html_element(.x, ".trackName.playIcon") %>% 
                 html_text2(),
@@ -63,10 +63,13 @@ for (i in 1:length(tracks_links)) {
               year = html_element(.x, ".tdata__td3:nth-child(4)") %>% 
                 html_text2(),
               genre = html_element(.x, ".tdata__badge") %>% 
-                html_text2() 
-            ))
-        } 
-      })
-      result <- bind_rows(result, extracted)
+                html_text2()
+            )))  
+      result<- bind_rows(result,extracted_data)
   }
-}
+
+
+
+
+
+
